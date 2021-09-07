2ed15b5e6149c46becd4bbdb8fa05d0cc042787b
Rujay <rujayw14@gmail.com>
2021-09-07 17:47:35 +0100

    unnecessary code comments removed

--- app.js
+++ app.js
@@ -21,19 +21,23 @@ app.use(mainRoutes);
 app.use((req, res, next) => {
     const err = new Error("Page not found");
     err.status = 404;
+    console.log('Page not found :(');
     res.render('not-found', {err})
 })
 
 //Global error handler middleware
 app.use((err, req, res, next) => {
    if (err) {
+       console.log('Something went wrong :(')
        console.log('Global error is called', err);
    }
    if (err.status === 404) {
+
        res.status(404).render('not-found', {err});
    } else {
        err.message = err.message || 'Something went wrong :(';
        res.status(err.status || 500).render('error', {err})
+       
    }
     
 });
